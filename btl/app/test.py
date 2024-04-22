2de893799a802794cfec7463d634ef488dc3e279
Giang Hong <gianghong1412@gmail.com>
2024-04-22 21:19:47 +0700

    them cau lenh

--- btl/app/test.py
+++ btl/app/test.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
+import pyspark.sql.functions as f
 import pyspark.pandas as ps
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -38,70 +39,73 @@ if __name__ == "__main__":
                                             .withColumn("year",splitDate.getItem(2))
 
     #analysis and visualization
-
-    #total number of sales each years
-    # totalNumberSale = dataSale \
-    #     .groupBy("year") \
-    #     .agg(sum("price").alias("total_price")) \
-    #     .orderBy("total_price", ascending = False)
+    print("tong doanh thu theo nam")
+    totalNumberSale = dataSale \
+        .groupBy("year") \
+        .agg(sum("price").alias("total_price")) \
+        .orderBy("total_price", ascending = False) \
+        .show()
     
-    #average sale per month, monthly revenue
-    
-    # aveSalePerMonth2021 = dataSale \
-    #     .filter(dataSale["year"]=="2021") \
-    #     .groupBy("month") \
-    #     .agg(avg("price").alias("average"), \
-    #          sum("price").alias("price")) \
-    #     .orderBy("average", ascending = False)
-    # aveSalePerMonth2021.show()
+    print("===doanh thu trung binh moi thang nam 2021===")
+    aveSalePerMonth2021 = dataSale \
+        .filter(dataSale["year"]=="2021") \
+        .groupBy("month") \
+        .agg(avg("price").alias("average"), \
+             sum("price").alias("price")) \
+        .orderBy("average", ascending = False)
+    aveSalePerMonth2021.show()
 
-    # aveSalePerMonth2022 = dataSale \
-    #     .filter(dataSale["year"]=="2022") \
-    #     .groupBy("month") \
-    #     .agg(avg("price").alias("average"), \
-    #          sum("price").alias("price")) \
-    #     .orderBy("average", ascending = False)
-    # aveSalePerMonth2022.show()
+    print("===doanh thu trung binh moi thang nam 2022===")
+    aveSalePerMonth2022 = dataSale \
+        .filter(dataSale["year"]=="2022") \
+        .groupBy("month") \
+        .agg(avg("price").alias("average"), \
+             sum("price").alias("price")) \
+        .orderBy("average", ascending = False)
+    aveSalePerMonth2022.show()
 
-    # key demographic of customer
+    print("==== count gender===")
     genderCount = dataSale \
         .groupBy("gender") \
         .agg(count("gender").alias("countg")) \
         .show()
-    schemaGenderCount = ("gender string,countg interger")
-    dfGenderCount = SparkSession.createDataFrame(genderCount,schemaGenderCount)
-    dpFrame = dfGenderCount.toPandas()
-    plt.figurhowe(figsize=(8,8))
-    plt.pie(dpFrame["countg"], \
-            labels=dpFrame["gender"], \
-            autopct="%1.2f%%", \
-            colors=["#FFB6D9","#99DBF5"])
-    plt.title("Male vs Female")
-    plt.legend()
-    plt.show()
     
-    # aveAgeGender = dataSale \
-    #     .groupBy("gender") \
-    #     .agg(avg("age").alias("age averange")) \
-    #     .show()
-
-    # avemarket the most sale
-    # totalPriceMarket = dataSale \
-    #     .groupBy("shopping_mall") \
-    #     .agg(sum("price").alias("total_price")) \
-    #     .orderBy("total_price", ascending = False).show()
-
-    # total by category
-    # totalPriceCategory = dataSale \
-    #     .groupBy("category") \
-    #     .agg(sum("price").alias("total_price"),sum("quantity").alias("total_quantity")) \
-    #     .orderBy("total_quantity", ascending = False).show()
+    # schemaGenderCount = ("gender string,countg interger")
+    # dfGenderCount = SparkSession.createDataFrame(genderCount,schemaGenderCount)
+    # dpFrame = dfGenderCount.toPandas()
+    # plt.figure(figsize=(8,8))
+    # plt.pie(dpFrame["countg"], \
+    #         labels=dpFrame["gender"], \
+    #         autopct="%1.2f%%", \
+    #         colors=["#FFB6D9","#99DBF5"])
+    # plt.title("Male vs Female")
+    # plt.legend()
+    # plt.show()
     
-    # print ("===add total_price each year")
-    # responseSplitDate \
-    #     .groupBy("year") \
-    #     .agg(sum("price").alias("total_price")) \
-    #     .orderBy("total_price", ascending = False)
+    print("=== do tuoi trung binh theo gioi tinh===")
+    aveAgeGender = dataSale \
+        .groupBy("gender") \
+        .agg(avg("age").alias("age averange")) \
+        .show()
+
+    print("===Doanh thu cua cac dia diem===")
+    totalPriceMarket = dataSale \
+        .groupBy("shopping_mall") \
+        .agg(sum("price").alias("total_price")) \
+        .orderBy("total_price", ascending = False).show()
+
+    print ("===Doanh thu cua tung mat hang===")
+    totalPriceCategory = dataSale \
+        .groupBy("category") \
+        .agg(sum("price").alias("total_price"),sum("quantity").alias("total_quantity")) \
+        .orderBy("total_price", ascending = False).show()
+    
+    print ("===Doanh thu hang nam===")
+    totalYear=dataSale \
+        .groupBy("year") \
+        .agg(sum("price").alias("total_price")) \
+        .orderBy("total_price", ascending = False) \
+        .show()
 
     # print ("===total price each category in 2022")
     # response2022 = responseSplitDate.filter(responseSplitDate['year'] == "2022") \
