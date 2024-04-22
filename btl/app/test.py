b51f60d1ba7cf6378072aa67e48d525d8fbd4a56
gianghong1412 <165347568+gianghong1412@users.noreply.github.com>
2024-04-22 11:28:00 +0700

    Update test.py

--- btl/app/test.py
+++ btl/app/test.py
@@ -1,70 +1,123 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import sum, split
-from pyspark.sql.types import IntegerType
-
-def getMonth(word: str):
-    month = word
-    if length(month) < 2 : month = "0" + month
-    return month
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 if __name__ == "__main__":
     session = SparkSession.builder.appName("test").getOrCreate()
     dataFrameReader = session.read
-
-    response = dataFrameReader \
+    data_Schema = [ \
+        StructField("invoice_no",StringType(),True), \
+        StructField("customer_id",StringType(),True), \
+        StructField("age",IntegerType(),True), \
+        StructField("category",StringType(),True), \
+        StructField("quantity",IntegerType(),True), \
+        StructField("price",DoubleType(),True), \
+        StructField("payment_method",StringType(),True), \
+        StructField("invoice_date",DateType(),True), \
+        StructField("shopping_mall",StringType(),True) \
+    ]
+    finalStruct = StructType(fields=data_Schema)
+    dataSale = dataFrameReader \
         .option("header","true") \
-        .option("inferSchema", value = True )\
-        .csv("btl/in/customer_shopping_data.csv")
+        .option("inferschema", True) \
+        .csv("in/customer_shopping_data.csv")
 
-    # print ("====Print schema===")
-    # response.printSchema()
+    print ("====Print schema===")
+    dataSale.printSchema()
 
     print ("===Print selected columns")
-    responSelectedColumns = response.select("category","quantity","price","invoice_date")
-    responSelectedColumns.show(100)
+    dataSale = dataSale.select("customer_id","gender","age","category","quantity","price","payment_method","invoice_date","shopping_mall")
+    splitDate = split(dataSale["invoice_date"],"/")
+    dataSale = dataSale.withColumn("month",splitDate.getItem(1).cast(IntegerType())) \
+                                            .withColumn("year",splitDate.getItem(2))
 
-    # print ("===total quantity of category")
+    #analysis and visualization
+
+    #total number of sales each years
+    # totalNumberSale = dataSale \
+    #     .groupBy("year") \
+    #     .agg(sum("price").alias("total_price")) \
+    #     .orderBy("total_price", ascending = False)
+    
+    #average sale per month, monthly revenue
+    
+    # aveSalePerMonth2021 = dataSale \
+    #     .filter(dataSale["year"]=="2021") \
+    #     .groupBy("month") \
+    #     .agg(avg("price").alias("average"), \
+    #          sum("price").alias("price")) \
+    #     .orderBy("average", ascending = False)
+    # aveSalePerMonth2021.show()
+
+    # aveSalePerMonth2022 = dataSale \
+    #     .filter(dataSale["year"]=="2022") \
+    #     .groupBy("month") \
+    #     .agg(avg("price").alias("average"), \
+    #          sum("price").alias("price")) \
+    #     .orderBy("average", ascending = False)
+    # aveSalePerMonth2022.show()
+
+    # key demographic of customer
+    genderCount = dataSale \
+        .groupBy("gender") \
+        .agg(count("gender").alias("countg")) \
+        .show()
+    plt.figure(figsize=(8,8))
+    plt.pie(genderCount["countg"], \
+            labels=genderCount["gender"], \
+            autopct="%1.2f%%", \
+            colors=["#FFB6D9","#99DBF5"])
+    plt.title("Male vs Female")
+    plt.legend()
+    plt.show()
+    
+    aveAgeGender = dataSale \
+        .groupBy("gender") \
+        .agg(avg("age").alias("age averange")) \
+        .show()
+
     # responSelectedColumns \
     #     .groupBy("Category") \
     #     .agg(sum("quantity").alias("total_quantity")) \
     #     .orderBy("total_quantity", ascending = False).show()
     
     # print ("===add date month year columns===")
-    splitDate = split(responSelectedColumns["invoice_date"],"/")
-    responseSplitDate = responSelectedColumns.withColumn("day",splitDate.getItem(0)) \
-                                            .withColumn("month",splitDate.getItem(1).cast(IntegerType())) \
-                                            .withColumn("year",splitDate.getItem(2))
+    # splitDate = split(responSelectedColumns["invoice_date"],"/")
+    # responseSplitDate = responSelectedColumns.withColumn("day",splitDate.getItem(0)) \
+                                            # .withColumn("month",splitDate.getItem(1).cast(IntegerType())) \
+                                            # .withColumn("year",splitDate.getItem(2))
     # responseSplitDate.show(truncate=False)
 
     # print ("===add total_price each year")
-    responseSplitDate \
-        .groupBy("year") \
-        .agg(sum("price").alias("total_price")) \
-        .orderBy("total_price", ascending = False)
+    # responseSplitDate \
+    #     .groupBy("year") \
+    #     .agg(sum("price").alias("total_price")) \
+    #     .orderBy("total_price", ascending = False)
 
-    print ("===total price each category in 2022")
-    response2022 = responseSplitDate.filter(responseSplitDate['year'] == "2022") \
-                    .groupBy("month") \
-                    .agg(sum("price").alias("total_price")) \
-                    .orderBy("total_price",ascending = False) \
+    # print ("===total price each category in 2022")
+    # response2022 = responseSplitDate.filter(responseSplitDate['year'] == "2022") \
+    #                 .groupBy("month") \
+    #                 .agg(sum("price").alias("total_price")) \
+    #                 .orderBy("total_price",ascending = False) \
                     
     
-    print ("===total price each category in 2021")
-    response2021 = responseSplitDate.filter(responseSplitDate['year'] == "2021") \
-                    .groupBy("month") \
-                    .agg(sum("price").alias("total_price")) \
-                    .orderBy("total_price",ascending = False) \
+    # print ("===total price each category in 2021")
+    # response2021 = responseSplitDate.filter(responseSplitDate['year'] == "2021") \
+    #                 .groupBy("month") \
+    #                 .agg(sum("price").alias("total_price")) \
+    #                 .orderBy("total_price",ascending = False) \
                     
-    response2021 \
-        .write.mode("overwrite") \
-        .option("header", "true") \
-        .csv("btl/out/2021.csv")
+    # response2021 \
+    #     .write.mode("overwrite") \
+    #     .option("header", "true") \
+    #     .csv("out/2021.csv")
     
-    response2022 \
-        .write \
-        .option("header", "true") \
-        .csv("btl/out/2022.csv")
+    # response2022 \
+    #     .write \
+    #     .option("header", "true") \
+    #     .csv("out/2022.csv")
 
     print ("=======================")
 session.stop()
-
