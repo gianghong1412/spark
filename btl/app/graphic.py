71f13956135af39f440b5a566b58110b4cd75bb1
giang hong <gianghong1412@gmail.com>
2024-04-24 16:47:59 +0700

    them

--- btl/app/graphic.py
+++ btl/app/graphic.py
@@ -3,19 +3,49 @@ from matplotlib import pyplot
 from collections import Counter
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sn
 
 if __name__ == "__main__":
-    df = pd.read_csv('out/totalPriceSale.csv/part-00000-10ba2a99-85b0-4134-8fd4-fd029dde5cc5-c000.csv')
-    df.plot.barh(x='year',y='total_price',color='blue')
-    plt.show()   
+    # dt = pd.read_csv('out/totalPriceSale.csv/part-00000-10ba2a99-85b0-4134-8fd4-fd029dde5cc5-c000.csv')
+    # plt.figure(figsize=(16,8))
+    # plt.bar(dt["year"],dt["total_price"],color="red")
+    # plt.xlabel("year")
+    # plt.ylabel("total_price")
+    # plt.title("Doanh thu ban hang cac nam")
+    # plt.show()   
 
+    # dt = pd.read_csv('out/genderCount/part-00000-a0b496b0-9328-4368-b0ab-a6ba29673b99-c000.csv')
+    # plt.pie(dt["count"], \
+    #         labels=dt["gender"], \
+    #         autopct="%1.2f%%", \
+    #         colors=["#FFB6D9","#99DBF5"])
+    # plt.title("Ti le khach hang nam va nu")
+    # plt.show()
 
-    dt = pd.read_csv('out/genderCount/part-00000-a0b496b0-9328-4368-b0ab-a6ba29673b99-c000.csv')
-    plt.pie(dt["count"], \
-            labels=dt["gender"], \
-            autopct="%1.2f%%", \
-            colors=["#FFB6D9","#99DBF5"])
-    plt.title("Male vs Female")
+    # dt = pd.read_csv('out/totalCategoryEachMonIn2022.csv/part-00000-363b2467-0645-48b8-9e1a-0e6346201b82-c000.csv')
+    # sn.barplot(data=dt,x=dt["month"],y=dt["total_price"],hue=dt["category"],palette=["red","yellow","green","blue","purple","pink","black","grey"])
+    # plt.xlabel("category")
+    # plt.ylabel("total_price")
+    # plt.title("Doanh thu loai hang hoa theo tung thang trong nam 2022")
+    # plt.show()
+
+    # dt = pd.read_csv('out/totalCategoryEachMonIn2021.csv/part-00000-363b2467-0645-48b8-9e1a-0e6346201b82-c000.csv')
+    # sn.barplot(data=dt,x=dt["month"],y=dt["total_price"],hue=dt["category"],palette=["red","yellow","green","blue","purple","pink","black","grey"])
+    # plt.xlabel("category")
+    # plt.ylabel("total_price")
+    # plt.title("Doanh thu loai hang hoa theo tung thang trong nam 2021")
+    # plt.show()
+
+    dt = pd.read_csv('out/totalPriceCategory/part-00000-229ac4fb-766c-4209-b4d0-ae01c4c08e64-c000.csv')
+    plt.figure(figsize=(16,8))
+    plt.bar(dt["category"],dt["total_price"],color="red")
+    plt.xlabel("category")
+    plt.ylabel("total_price")
+    plt.title("Doanh thu ban hang cua tung loai mat hang")
     plt.show()
 
-    
+    plt.figure(figsize=(8,8))
+    plt.pie(dt["total_price"],labels=dt["category"],autopct="%1.2f%%")
+    plt.title("ti le doanh thu giua cac loai mat hang")
+    plt.legend(loc=(1.1,.5))
+    plt.show()
--- btl/app/test.py
+++ btl/app/test.py
@@ -12,25 +12,17 @@ import numpy as np
 if __name__ == "__main__":
     session = SparkSession.builder.appName("test").getOrCreate()
     dataFrameReader = session.read
-    # data_Schema = [ 
-    #     StructField("invoice_no",StringType(),True), 
-    #     StructField("customer_id",StringType(),True), 
-    #     StructField("age",IntegerType(),True), 
-    #     StructField("category",StringType(),True), 
-    #     StructField("quantity",IntegerType(),True), 
-    #     StructField("price",FloatType(),True), 
-    #     StructField("payment_method",StringType(),True), 
-    #     StructField("invoice_date",DateType(),True), 
-    #     StructField("shopping_mall",StringType(),True) ]
+
     data_Schema = StructType() \
         .add("invoice_no",StringType(),True) \
         .add("customer_id",StringType(),True) \
+        .add("gender",StringType(),True) \
         .add("age",IntegerType(),True) \
         .add("category",StringType(),True) \
         .add("quantity",IntegerType(),True) \
         .add("price",FloatType(),True) \
         .add("payment_method",StringType(),True) \
-        .add("invoice_date",DateType(),True) \
+        .add("invoice_date",StringType(),True) \
         .add("shopping_mall",StringType(),True)
     # finalStruct = StructType(data_Schema)
     dataSale = dataFrameReader \
@@ -42,10 +34,13 @@ if __name__ == "__main__":
     dataSale.printSchema()
 
     print ("===Print selected columns")
+    dataSale.withColumn("invoice_date",to_date(col("invoice_date"),"dd/mm/yyyy"))
     dataSale = dataSale.select("customer_id","gender","age","category","quantity","price","payment_method","invoice_date","shopping_mall")
+
     splitDate = split(dataSale["invoice_date"],"/")
     dataSale = dataSale.withColumn("month",splitDate.getItem(1).cast(IntegerType())) \
-                                            .withColumn("year",splitDate.getItem(2))
+                        .withColumn("year",splitDate.getItem(2))
+    # dataSale = dataSale.distinct()
 
     #analysis and visualization
     # print("tong doanh thu theo nam")
@@ -57,23 +52,21 @@ if __name__ == "__main__":
     #     .option("header", "true") \
     #     .csv("out/totalPriceSale")
     
-    print("===doanh thu trung binh moi thang nam 2021===")
-    aveSalePerMonth2021 = dataSale \
-        .filter(dataSale["year"]=="2021") \
+    print("===doanh thu trung binh moi thang ===")
+    totalSalePerMonth = dataSale \
         .groupBy("month") \
         .agg(sum("price").alias("total_price")) \
         .orderBy("month", ascending = False)
-    aveSalePerMonth2021.show()
+    aveSalePerMonth = totalSalePerMonth.agg(avg("total_price").alias("average of month")).show()
 
-    print("===doanh thu trung binh moi thang nam 2022===")
-    aveSalePerMonth2022 = dataSale \
-        .filter(dataSale["year"]=="2022") \
-        .groupBy("month") \
+    print("===doanh thu moi thang theo nam=== ")
+    totalSalePerMonthInYear = dataSale \
+        .groupBy("year","month") \
         .agg(sum("price").alias("total_price")) \
-        .orderBy("month", ascending = False)
-    aveSalePerMonth2022.show()
+        .orderBy("total_price", ascending = False)
+    totalSalePerMonthInYear.show(50)
 
-    # print("==== count gender===")
+    # print("====ti le khach hang nam va nu===")
     # genderCount = dataSale \
     #     .groupBy("gender") \
     #     .agg(count("gender").alias("count")) \
@@ -81,37 +74,64 @@ if __name__ == "__main__":
     #     .option("header", "true") \
     #     .csv("out/genderCount")
     
-    print("=== do tuoi trung binh theo gioi tinh===")
-    aveAgeGender = dataSale \
-        .groupBy("gender") \
-        .agg(avg("age").alias("age averange")) \
-        .show()
+    # print("=== do tuoi trung binh theo gioi tinh===")
+    # aveAgeGender = dataSale \
+    #     .groupBy("gender") \
+    #     .agg(avg("age").alias("age averange")) \
+    #     .show()
     
-    print("=== do tuoi khach hang===")
-    ageCount = dataSale \
-        .groupBy("age") \
-        .count() \
-        .show()
+    # print("=== do tuoi khach hang===")
+    # ageCount = dataSale \
+    #     .groupBy("age") \
+    #     .count() \
+    #     .show()
 
-    print("===Doanh thu cua cac dia diem===")
-    totalPriceMarket = dataSale \
-        .groupBy("shopping_mall") \
-        .agg(sum("price").alias("total_price")) \
-        .orderBy("total_price", ascending = False).show()
+    print ("===so luong loai mat hang theo gioi tinh")
+    quantityCategoryByGender = dataSale \
+        .groupBy("gender","category") \
+        .agg(sum("quantity").alias("total_quantity")) \
+        .orderBy("category","gender") \
+        .show(50)
+    
+    quantityCategoryByGender.write.mode("overwrite") \
+        .option("header", "true") \
+        .csv("out/quantityCategoryByGender")
+
+    # print("===Doanh thu cua cac dia diem===")
+    # totalPriceMarket = dataSale \
+    #     .groupBy("shopping_mall") \
+    #     .agg(sum("price").alias("total_price")) \
+    #     .orderBy("total_price", ascending = False).show()
 
     print ("===Doanh thu cua tung mat hang===")
     totalPriceCategory = dataSale \
         .groupBy("category") \
         .agg(sum("price").alias("total_price"),sum("quantity").alias("total_quantity")) \
-        .orderBy("total_price", ascending = False).show()
+        .orderBy("total_price", ascending = False)
+    totalPriceCategory.write.mode("overwrite") \
+                            .option("header", "true") \
+                            .csv("out/totalPriceCategory")
     
-    # print ("===total price each category in 2022")
-    # response2022 = responseSplitDate.filter(responseSplitDate['year'] == "2022") \
-    #                 .groupBy("month") \
+    # print ("===doanh thu tung loai hang theo thang trong 2022")
+    # totalCategoryEachMonIn2022 = dataSale.filter(dataSale['year'] == "2022") \
+    #                 .groupBy("month","category") \
     #                 .agg(sum("price").alias("total_price")) \
-    #                 .orderBy("total_price",ascending = False) \
-                    
+    #                 .orderBy("month",ascending = False)
+                       
+    # totalCategoryEachMonIn2022.write.mode("overwrite") \
+    #                         .option("header", "true") \
+    #                         .csv("out/totalCategoryEachMonIn2022.csv")
     
+    # print ("===doanh thu tung loai hang theo thang trong 2021")
+    # totalCategoryEachMonIn2022 = dataSale.filter(dataSale['year'] == "2021") \
+    #                 .groupBy("month","category") \
+    #                 .agg(sum("price").alias("total_price")) \
+    #                 .orderBy("month",ascending = False)
+                       
+    # totalCategoryEachMonIn2022.write.mode("overwrite") \
+    #                         .option("header", "true") \
+    #                         .csv("out/totalCategoryEachMonIn2021.csv")
+                    
     # print ("===total price each category in 2021")
     # response2021 = responseSplitDate.filter(responseSplitDate['year'] == "2021") \
     #                 .groupBy("month") \
