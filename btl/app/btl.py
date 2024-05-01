0e04ff9c4f346d8a02f5262dffb310b504a78495
Giang Hong <gianghong1412@gmail.com>
2024-05-01 23:21:33 +0700

    lam btl

---
+++ btl/app/btl.py
@@ -0,0 +1,94 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+import pyspark.sql.functions as f
+from datetime import datetime
+
+if __name__ == "__main__":
+    session = SparkSession.builder.appName("btl").getOrCreate()
+    dataFrameReader = session.read
+    data_Schema = StructType() \
+        .add("InvoiceNo",StringType(),True) \
+        .add("StockCode",StringType(),True) \
+        .add("Description",StringType(),True) \
+        .add("Quantity",IntegerType(),True) \
+        .add("InvoiceDate",DateType(),True) \
+        .add("UnitPrice",FloatType(),True) \
+        .add("CustomerID",StringType(),True) \
+        .add("Country",StringType(),True) 
+        
+    dataSale = dataFrameReader \
+        .option("header","true") \
+        .option('inferschema',True) \
+        .csv("in/Online Retail.csv") 
+    print ("====Print schema===")
+    dataSale.printSchema()
+    dataSale.describe().show()
+    dataSale.na.drop()
+    dataSale =  dataSale \
+        .filter(dataSale['Quantity']>0) \
+        .filter(dataSale['UnitPrice']>0)
+
+    # splitDate = split(dataSale["InvoiceDate"],"/")
+    # dataSale = dataSale.withColumn("month",splitDate.getItem(0).cast(IntegerType())) \
+    #                     .withColumn("year",splitDate.getItem(2))
+
+    # print("tong doanh thu theo nam")
+    # totalPriceSale = dataSale \
+    #     .groupBy("year") \
+    #     .agg(round(sum(dataSale['UnitPrice']*dataSale["Quantity"]),2).alias("total_price")) \
+    #     .orderBy("year", ascending = False) \
+    #     .write.mode("overwrite") \
+    #     .option("header", "true") \
+    #     .csv("outBTL/totalPriceSale")
+    
+    # print("===doanh thu moi thang===")
+    # totalSalePerMonthInYear = dataSale \
+    #     .groupBy("year","month") \
+    #     .agg(round(sum(dataSale['UnitPrice']*dataSale["Quantity"]),2).alias("total_price")) \
+    #     .orderBy("year","month", ascending = True) \
+    #     .withColumn("timeData",concat_ws("/",col("month"),col("year"))) \
+    #     .write.mode("overwrite") \
+    #     .option("header", "true") \
+    #     .csv("outBTL/totalSalePerMonthInYear")
+
+    # print("===Doanh thu cua cac nuoc===")
+    # totalPriceMarket = dataSale \
+    #     .groupBy("Country") \
+    #     .agg(round(sum(dataSale['UnitPrice']*dataSale["Quantity"]),2).alias("total_price")) \
+    #     .orderBy("total_price", ascending = False) \
+    #     .write.mode("overwrite") \
+    #     .option("header", "true") \
+    #     .csv("outBTL/totalPriceMarket")
+
+    print ("===Doanh thu cua tung mat hang===")
+    totalPriceCategory = dataSale \
+        .groupBy("StockCode") \
+        .agg(round(sum(dataSale['UnitPrice']*dataSale["Quantity"]),2).alias("total_price")) \
+        .orderBy("total_price", ascending = False) \
+        .write.mode("overwrite") \
+        .option("header", "true") \
+        .csv("outBTL/totalPriceCategory")
+
+    dataCustomerCount = dataSale \
+        .groupBy("CustomerID") \
+        .agg(count_distinct("InvoiceNo").alias('frequency')) \
+        .write.mode("overwrite") \
+        .option("header", "true") \
+        .csv("outBTL/dataCustomerCount")
+    
+    # dataCustomerAmount = dataSale \
+    #     .groupBy("CustomerID") \
+    #     .agg(round(sum(dataSale['UnitPrice']*dataSale["Quantity"]),2).alias("total_price")) \
+    #     .write.mode("overwrite") \
+    #     .option("header", "true") \
+    #     .csv("outBTL/dataCustomerAmount")
+
+    # dataCustomerRecency = dataSale \
+    #     .select("CustomerID","InvoiceDate") \
+    #     .write.mode("overwrite") \
+    #     .option("header", "true") \
+    #     .csv("outBTL/dataCustomerRecency")
+    
+        
+
--- btl/app/graphic.py
+++ btl/app/graphic.py
@@ -5,6 +5,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sn
 import plotly.express as px
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+from kmodes.kprototypes import KPrototypes
 
 if __name__ == "__main__":
         # dt = pd.read_csv('out/totalPriceSale/totalPriceSale.csv')
@@ -79,8 +83,6 @@ if __name__ == "__main__":
         # plt.xticks(dt["category"],fontsize=10,rotation=45)
         # plt.show() 
 
-
-
         # dt = pd.read_csv('out/totalPriceMarket/totalPriceMarket.csv')
         # plt.figure(figsize=(16,8))
         # plt.bar(dt["shopping_mall"],dt["total_price"],color="red")
@@ -92,31 +94,111 @@ if __name__ == "__main__":
         # plt.title("Doanh thu theo shopping mall")
         # plt.show() 
 
+        # dt = pd.read_csv('out/totalPriceCategory/totalPriceCategory.csv')
+        # plt.figure(figsize=(16,8))
+        # plt.bar(dt["category"],dt["total_price"],color="red")
+        # plt.xlabel("category")
+        # plt.ylabel("total_price")
+        # plt.title("Doanh thu ban hang cua tung loai mat hang")
+        # plt.show()
 
-    # dt = pd.read_csv('out/totalCategoryEachMonIn2022.csv/part-00000-363b2467-0645-48b8-9e1a-0e6346201b82-c000.csv')
-    # sn.barplot(data=dt,x=dt["month"],y=dt["total_price"],hue=dt["category"],palette=["red","yellow","green","blue","purple","pink","black","grey"])
-    # plt.xlabel("category")
-    # plt.ylabel("total_price")
-    # plt.title("Doanh thu loai hang hoa theo tung thang trong nam 2022")
-    # plt.show()
+        # plt.figure(figsize=(8,8))
+        # plt.pie(dt["total_price"],labels=dt["category"],autopct="%1.2f%%")
+        # plt.title("ti le doanh thu giua cac loai mat hang")
+        # plt.legend(loc=(1.1,.5))
+        # plt.show()
+    dt = pd.read_csv('in/customer_shopping_data.csv')
+    dt_new = dt.drop(['invoice_no','customer_id','category','gender','payment_method','invoice_date','shopping_mall'],axis=1,inplace=True)
+    dt_new = pd.DataFrame(dt_new)
+    # dt_new.info()
+    # sn.pairplot(dt.iloc[:,2:])
+    # scaled_dt = dt.copy()
+    # columnToNomalize = ['age','quantity','price']
+    # scaled_dt[columnToNomalize] = scaled_dt[columnToNomalize].apply(lambda x: (x-x.mean())/np.std(x))
+    # scaled_dt = scaled_dt.iloc[:,1:]
 
-    # dt = pd.read_csv('out/totalCategoryEachMonIn2021.csv/part-00000-363b2467-0645-48b8-9e1a-0e6346201b82-c000.csv')
-    # sn.barplot(data=dt,x=dt["month"],y=dt["total_price"],hue=dt["category"],palette=["red","yellow","green","blue","purple","pink","black","grey"])
-    # plt.xlabel("category")
-    # plt.ylabel("total_price")
-    # plt.title("Doanh thu loai hang hoa theo tung thang trong nam 2021")
-    # plt.show()
+    # cost = []
+    # for num_clusters in list(range(1,14)):
+    #     kproto = KPrototypes(n_clusters=num_clusters,init ='huang')
+    #     kproto.fit_predict(scaled_dt,categorical=[0])
+    #     cost.append(kproto.cost_)
+    # plt.plot(cost)
+    # plt.xlabel('Number of cluster')
 
-        dt = pd.read_csv('out/totalPriceCategory/totalPriceCategory.csv')
-        plt.figure(figsize=(16,8))
-        plt.bar(dt["category"],dt["total_price"],color="red")
-        plt.xlabel("category")
-        plt.ylabel("total_price")
-        plt.title("Doanh thu ban hang cua tung loai mat hang")
-        plt.show()
+    # kmean = KMeans(n_clusters=4,max_iter=50,n_init=10)
+    # kmean.fit(dt)
+    # kmean.labels_
+    # ssd = []
+    # range_n_clusters = [2,3,4,5,6,7,8]
+    # for num_cluster in range_n_clusters:
+    #     kmean = KMeans(n_clusters=num_cluster,max_iter=50,n_init=10)
+    #     kmean.fit(dt)
+    #     ssd.append(kmean.inertia_)
+    #     print("For n_cluster={0}, the Elbow score is {1}".format(num_cluster,kmean.inertia_))
 
-        plt.figure(figsize=(8,8))
-        plt.pie(dt["total_price"],labels=dt["category"],autopct="%1.2f%%")
-        plt.title("ti le doanh thu giua cac loai mat hang")
-        plt.legend(loc=(1.1,.5))
-        plt.show()
+    # fig = px.line(x=range_n_clusters,y =ssd,
+    #               title="Elbow cuver for K-mean Clustering",
+    #               labels={'x':'Number of Cluster','y':'Sum of Square Distances SSD'})
+    # fig.update_layout(
+    #     xaxis=dict(title_font=dict(size=14)),
+    #     yaxis=dict(title_font=dict(size=14)),
+    #     showlegend=False,
+    #     width=800,
+    #     height=600
+    # )
+    # fig.show()
+        # silhouette_avg = silhouette_score(dt,kmean.labels_)
+        # print("For n_cluster={0}, the silhouette is {1}".format(num_cluster,silhouette_avg))
+    kmean = KMeans(n_clusters=3,max_iter=50,n_init=10)
+    kmean.fit(dt)
+    dt["cluter_id"] = kmean.labels_
+    dt.info()
+
+    fig = px.box(dt,x='cluter_id',y='price',title="Cluster Id and price",
+                 labels={'cluter_id': 'Cum','price':'Chi tieu'},
+                 color='cluter_id')
+    fig.update_layout(
+        xaxis=dict(title="Cum",title_font=dict(size=14)),
+        yaxis=dict(title="Chi tieu",title_font=dict(size=14)),
+        showlegend=False,
+        width=800,
+        height=600)
+    fig.show()
+
+    # fig = px.box(dt,x='cluter_id',y='age',title="Cluster Id and price",
+    #              labels={'cluter_id': 'Cum','price':'Tuoi'},
+    #              color='cluter_id')
+    # fig.update_layout(
+    #     xaxis=dict(title="Cum",title_font=dict(size=14)),
+    #     yaxis=dict(title="Chi tieu",title_font=dict(size=14)),
+    #     showlegend=False,
+    #     width=800,
+    #     height=600)
+    # fig.show()
+
+    # fig = px.box(dt,x='cluter_id',y='quantity',title="Cluster Id and price",
+    #              labels={'cluter_id': 'Cum','price':'So luong dat hang'},
+    #              color='cluter_id')
+    # fig.update_layout(
+    #     xaxis=dict(title="Cum",title_font=dict(size=14)),
+    #     yaxis=dict(title="Chi tieu",title_font=dict(size=14)),
+    #     showlegend=False,
+    #     width=800,
+    #     height=600)
+    # fig.show()
+
+    fig = px.scatter(dt,x='cluter_id',y='price', color='quantity',title="Cluster Id and price",
+                 labels={'cluter_id': 'Cum','price':'Chi tieu','quantity':'Soluong'}
+                 )
+    fig.update_layout(
+        xaxis=dict(title="Cum",title_font=dict(size=14)),
+        yaxis=dict(title="Chi tieu",title_font=dict(size=14)),
+        showlegend=False,
+        width=800,
+        height=600)
+    fig.show()
+
+
+
+    print("=============================")
+
